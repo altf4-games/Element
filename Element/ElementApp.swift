877d102b265d4bb9474d76526ffa4d2842e1594c
Pradyum Mistry <39883432+altf4-games@users.noreply.github.com>
2026-08-25 20:12:31 +0530

    Initial Commit

---
+++ Element/ElementApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElementApp.swift
+//  Element
+//
+//  Created by Pradyum Mistry on 25/08/26.
+//
+
+import SwiftUI
+
+@main
+struct ElementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
